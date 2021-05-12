a4f80d0c06b7ee6e209fa45a7fb1de27a8200bea
Brandon Gonzales <gonzobrandon@gmail.com>
2021-05-12 08:39:30 -0600

    no db in php index

--- app/index.php
+++ app/index.php
@@ -2,35 +2,45 @@
 <head></head>
 <body>
 	Hello this is your application<br/>
-<?php
 
-  $servername = "HOSTNAME";
-  $username = "USERNAME";
-  $password = "PASSWORD";
-  $database = "DATABASE"
-  $port = 3306
 
-  // Create connection
-  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-  $conn = new mysqli($servername, $username, $password, $database, $port);
+  <?php
 
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  } 
-  
-  $query = "SELECT * FROM employees WHERE emp_no = 1;";
-  
-  $result = mysqli_query($conn, $query);
+// $servername = "HOSTNAME";
+// $username = "USERNAME";
+// $password = "PASSWORD";
+// $database = "DATABASE"
+// $port = 3306
 
-  /* fetch associative array */
-  while ($row = mysqli_fetch_assoc($result)) {
-    printf("Hello, %s %s\n", $row['first_name'], $row['last_name']);
-  }
+// // $servername = "localhost";
+// // $username = "root";
+// // $password = "root";
+// // $database = "employees";
+// // $port = 3306;
+
+
+// // Create connection
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// $conn = new mysqli($servername, $username, $password, $database, $port);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   echo "Error connecting";
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+
+// $query = "SELECT * FROM employees WHERE emp_no = 1;";
+
+// $result = mysqli_query($conn, $query);
+
+// /* fetch associative array */
+// while ($row = mysqli_fetch_assoc($result)) {
+//   printf("Hello, %s %s\n", $row['first_name'], $row['last_name']);
+// }
 
 
 ?>
-
 
 
 </body>
